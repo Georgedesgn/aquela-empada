e3bed573b907af92a71a3c3d0688b6e89c7ed86a
Georgedesgn <georgedesign.es@outlook.com>
2025-05-20 15:13:56 -0300

    Create AquelaEmpadaApplication.java

---
+++ src/main/java/com/aquelaempada/AquelaEmpadaApplication.java
@@ -0,0 +1,11 @@
+package com.aquelaempada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquelaEmpadaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AquelaEmpadaApplication.class, args);
+    }
+}
